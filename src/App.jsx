efa19031c66ed5a3937c81405944b16e2ee437b4
teymur <teymurhasanov100@gmail.com>
2023-10-18 16:36:24 +0400

    message

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { Link, Route,Routes } from 'react-router-dom'
 import { Home } from './pages/Home'
 import { BookList } from './pages/BookList'
+import { Book } from './pages/Book'
 function App() {
   return(
   <>
@@ -17,6 +18,7 @@ function App() {
     <Routes>
       <Route path="/" element={<Home />}  />
       <Route path='/books' element={<BookList />}/>
+      <Route path='/books/:id' element={<Book />}/>
     </Routes>
   </>
   )
--- src/pages/BookList.jsx
+++ src/pages/BookList.jsx
@@ -1,5 +1,9 @@
 export function BookList() {
-  return <h1>BookList</h1>
+  return(
+    <>
+      <h1>BookList</h1>
+    </>
+  )
 }
 
 
